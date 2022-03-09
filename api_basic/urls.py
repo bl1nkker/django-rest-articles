f58a77f0d431f1b168df030dc2a3da4df18a6bf9
bl1nkker <nurovich14@gmail.com>
2022-03-10 00:10:59 +0600

    feat: generic api view

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ArticleDetailsAPIView, article_details, article_list, ArticleAPIView
+from .views import ArticleDetailsAPIView, GenericAPIView, article_details, article_list, ArticleAPIView
 
 urlpatterns = [
     # path('articles/', article_list),
     # path('articles/<int:pk>', article_details)
+
     path('articles/', ArticleAPIView.as_view()),
-    path('articles/<int:pk>', ArticleDetailsAPIView.as_view())
+    # path('articles/<int:pk>', ArticleDetailsAPIView.as_view())
+
+    path('articles/<int:id>', GenericAPIView.as_view()),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -6,9 +6,34 @@ from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
+ 
+# Generic Based Views
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'
 
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+    
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
+    
+    
+    
+
+# Class Based Views
 class ArticleAPIView(APIView):
     def get(self, request):
         articles = Article.objects.all()
@@ -49,7 +74,7 @@ class ArticleDetailsAPIView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
+# Fucntional Based Views
 @api_view(['GET', 'POST'])
 def article_list(request):
     if request.method == 'GET':
